ea4aae3898c63f66529c4b9b393e94ff9d9a7e66
myungjuice <wkdaudwn1028@gmail.com>
2025-07-05 01:48:00 +0900

    chore: icon 관련 세팅

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,9 @@
 import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
+import svgr from "vite-plugin-svgr";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
-  plugins: [react(), tsconfigPaths(), tailwindcss()],
+  plugins: [react(), tsconfigPaths(), tailwindcss(), svgr()],
 });
