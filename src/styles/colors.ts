ef79e39247d1abca92ce4ebe87930fddf6631c2a
myungjuice <wkdaudwn1028@gmail.com>
2025-07-04 19:56:32 +0900

    chore: color, typography 정의

---
+++ src/styles/colors.ts
@@ -0,0 +1,11 @@
+const colors = {
+  transparent: "transparent",
+  primary: "#4880EE",
+  red: "#E84118",
+  gray: "#DADADA",
+  "light-gray": "#F2F4F6",
+  white: "#FFFFFF",
+  black: "#222222",
+};
+
+export default colors;
---
+++ src/styles/typography.ts
@@ -0,0 +1,68 @@
+const typography = {
+  "title-1": [
+    "24px",
+    {
+      fontWeight: 700,
+      lineHeight: "24px",
+      letterSpacing: "0%",
+    },
+  ],
+  "title-2": [
+    "22px",
+    {
+      fontWeight: 700,
+      lineHeight: "24px",
+      letterSpacing: "0%",
+    },
+  ],
+  "title-3": [
+    "18px",
+    {
+      fontWeight: 700,
+      lineHeight: "18px",
+      letterSpacing: "0%",
+    },
+  ],
+  "body-1": [
+    "20px",
+    {
+      fontWeight: 500,
+      lineHeight: "20px",
+      letterSpacing: "0%",
+    },
+  ],
+  "body-2": [
+    "14px",
+    {
+      fontWeight: 500,
+      lineHeight: "14px",
+      letterSpacing: "0%",
+    },
+  ],
+  "body-2-bold": [
+    "14px",
+    {
+      fontWeight: 700,
+      lineHeight: "14px",
+      letterSpacing: "0%",
+    },
+  ],
+  caption: [
+    "16px",
+    {
+      fontWeight: 500,
+      lineHeight: "16px",
+      letterSpacing: "0%",
+    },
+  ],
+  small: [
+    "10px",
+    {
+      fontWeight: 500,
+      lineHeight: "10px",
+      letterSpacing: "0%",
+    },
+  ],
+};
+
+export default typography;
