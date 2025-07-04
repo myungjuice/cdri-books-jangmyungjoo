6daf395c6553676f36bdc20cd24c2a9d06f7e81c
myungjuice <wkdaudwn1028@gmail.com>
2025-07-04 20:54:45 +0900

    feat: Button 컴포넌트 추가

--- src/styles/colors.ts
+++ src/styles/colors.ts
@@ -3,9 +3,13 @@ const colors = {
   primary: "#4880EE",
   red: "#E84118",
   gray: "#DADADA",
+  "gray-2": "#B1B8C0",
   "light-gray": "#F2F4F6",
   white: "#FFFFFF",
   black: "#222222",
+  "text-sub-title": "#8D94A0",
+  "text-primary": "#353C49",
+  "text-secondary": "#6D7582",
 };
 
 export default colors;
