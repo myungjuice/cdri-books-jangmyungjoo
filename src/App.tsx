2fb6ce9549662cdefb8d9a2b830a79c2f45f4c49
myungjuice <wkdaudwn1028@gmail.com>
2025-07-04 13:40:52 +0900

    chore: react-query 세팅

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,20 @@
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { Suspense } from "react";
 
 import { Routes } from "@/pages/Routes";
 
+const queryClient = new QueryClient();
+
 export default function App() {
   return (
     <>
-      <Suspense fallback={<div>로딩 중...</div>}>
-        <Routes />
-      </Suspense>
+      <QueryClientProvider client={queryClient}>
+        <ReactQueryDevtools initialIsOpen={false} />
+        <Suspense fallback={<div>로딩 중...</div>}>
+          <Routes />
+        </Suspense>
+      </QueryClientProvider>
     </>
   );
 }
