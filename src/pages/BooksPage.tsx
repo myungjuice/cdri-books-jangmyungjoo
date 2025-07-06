6da89d7baf9a868a59b72bb2a7bce60a46720653
myungjuice <wkdaudwn1028@gmail.com>
2025-07-07 05:39:37 +0900

    chore: 페이지별 브라우저 Title 설정

--- src/pages/BooksPage.tsx
+++ src/pages/BooksPage.tsx
@@ -1,13 +1,20 @@
+import { Helmet } from "react-helmet-async";
+
 import PageTitle from "@/components/shared/PageTitle";
 import BookListSection from "@/domains/books/components/BookListSection";
 import BookSearchSection from "@/domains/books/components/BookSearchSection";
 
 export default function BooksPage() {
   return (
-    <div className="flex flex-col gap-4">
-      <PageTitle>도서 검색</PageTitle>
-      <BookSearchSection />
-      <BookListSection />
-    </div>
+    <>
+      <Helmet>
+        <title>[CDRI] 도서 검색</title>
+      </Helmet>
+      <div className="flex flex-col gap-4">
+        <PageTitle>도서 검색</PageTitle>
+        <BookSearchSection />
+        <BookListSection />
+      </div>
+    </>
   );
 }
--- src/pages/WishlistPage.tsx
+++ src/pages/WishlistPage.tsx
@@ -1,11 +1,18 @@
+import { Helmet } from "react-helmet-async";
+
 import PageTitle from "@/components/shared/PageTitle";
 import BookListSection from "@/domains/wishlist/components/BookListSection";
 
 export default function WishlistPage() {
   return (
-    <div>
-      <PageTitle>내가 찜한 책</PageTitle>
-      <BookListSection />
-    </div>
+    <>
+      <Helmet>
+        <title>[CDRI] 내가 찜한 책</title>
+      </Helmet>
+      <div>
+        <PageTitle>내가 찜한 책</PageTitle>
+        <BookListSection />
+      </div>
+    </>
   );
 }
