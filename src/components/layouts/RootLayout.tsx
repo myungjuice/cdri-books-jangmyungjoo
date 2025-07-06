d09dd0b52238a2ba7b56c8f820ae7118195f3777
Myungjoo Jang <wkdaudwn1028@gmail.com>
2025-07-06 18:13:13 +0900

    feat: Header 컴포넌트 추가 (#65)
    * feat: Header 컴포넌트 추가
    
    * fix: header 높이 수정

--- src/components/layouts/RootLayout.tsx
+++ src/components/layouts/RootLayout.tsx
@@ -1,8 +1,11 @@
 import { Outlet } from "react-router-dom";
 
+import Header from "./Header";
+
 export default function RootLayout() {
   return (
     <div className="flex min-h-screen w-full flex-col items-center">
+      <Header />
       <main className="mx-auto w-full max-w-[960px] flex-1 py-20">
         <Outlet />
       </main>
