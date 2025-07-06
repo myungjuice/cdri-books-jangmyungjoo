aaa40965d0e31124e71a06a17b79fbc1dfc5f919
Myungjoo Jang <wkdaudwn1028@gmail.com>
2025-07-07 05:40:59 +0900

    chore: 페이지별 브라우저 Title 설정 (#78)

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { Suspense } from "react";
+import { HelmetProvider } from "react-helmet-async";
 
 import { Routes } from "@/pages/Routes";
 
@@ -10,13 +11,13 @@ const queryClient = new QueryClient();
 
 export default function App() {
   return (
-    <>
+    <HelmetProvider>
       <QueryClientProvider client={queryClient}>
         {import.meta.env.MODE !== "production" && <ReactQueryDevtools initialIsOpen={false} />}
         <Suspense fallback={<div>로딩 중...</div>}>
           <Routes />
         </Suspense>
       </QueryClientProvider>
-    </>
+    </HelmetProvider>
   );
 }
